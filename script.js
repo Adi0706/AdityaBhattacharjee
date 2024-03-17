6bb6c2d41b34d927443179aa830c515ceeea9cdd
Aditya Bhattacharjee <adityab76@gmail.com>
2024-03-17 14:32:20 +0530

    project page added

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
         el: document.querySelector('.main'),
         smooth: true
     });
+    const scroll2 = new LocomotiveScroll({
+        el: document.querySelector('.main2'),
+        smooth: true
+    });
 });
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
